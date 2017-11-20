15256a44179bcd7ba05bb6faa9f2d6a59d37ae59
Михаил <IneyQQ@gmail.com>
2017-11-20 18:05:06 +0300

    JavaScript

--- Kurs/mainwindow.cpp
+++ Kurs/mainwindow.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QtWebEngineWidgets>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    QWidget *mainWidg = new QWidget;
+    QVBoxLayout *vbl = new QVBoxLayout;
+    view = new QWebEngineView();
+    QPushButton *pb = new QPushButton("Push me");
+    vbl->addWidget(view);
+    vbl->addWidget(pb);
+    mainWidg->setLayout(vbl);
     ui->setupUi(this);
+    view->load(QUrl("http://www.qt.io/"));
+    view->load(QUrl("file:///home/misha/Programs/Kursach/Kurs/test.html"));
+    this->setCentralWidget(mainWidg);
+    connect(pb, SIGNAL(clicked(bool)), this, SLOT(runJs()));
 }
 
 MainWindow::~MainWindow()
--- Kurs/mainwindow.h
+++ Kurs/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtWebEngineWidgets>
 
 namespace Ui {
 class MainWindow;
@@ -12,8 +13,14 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    QWebEngineView *view;
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+protected slots:
+    void runJs() {
+        QWebEnginePage *page = view->page();
+        page->runJavaScript("alert(document.getElementById('1').value);", [](const QVariant &v) { qDebug() << v.toString(); });
+    }
 
 private:
     Ui::MainWindow *ui;
